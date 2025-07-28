ff5689315dfc4123cb076e97db7b26975e30efc9
siddheshtodkar <stodkar53@gmail.com>
2025-07-28 15:27:51 +0530

    state

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react"
+
 const Rating = () => {
+    const [rating, setRating] = useState(0)
+    const [hover, setHover] = useState(0)
+    const feedback = ['Terrible', 'Poor', 'Fair', 'Good', 'Excellent']
+    const stars = Array.from({ length: 5 }, (_, i) => i + 1)
     return (
         <div className="rating-container">
             <h2>Rate your Experiance</h2>
+            <div className="stars">
+                {
+                    stars.map(star => (
+                        <span key={star} className={`star ${(hover || rating) >= star && 'active'}`}
+                            onClick={() => setRating(star)}
+                            onMouseEnter={() => setHover(star)}
+                            onMouseLeave={() => setHover(0)}>
+                            {'\u2605'}
+                        </span>
+                    ))
+                }
+            </div>
+            <div className="feedback">
+                {feedback[rating - 1]}
+            </div>
         </div>
     )
 }
